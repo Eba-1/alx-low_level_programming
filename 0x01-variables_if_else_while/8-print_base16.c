8ac69d936242774c5b650d99ec043ec204b500d1
Eba-1 <ebenwoldehan@gmail.com>
2023-02-17 19:57:13 -0800

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int i;
+	char ch = 'a';
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	for (i = 0; i < 6; i++)
+	{
+		putchar('ch');
+		ch++;	
+	}
+	putchar('\n');
+	return (0);
+}
