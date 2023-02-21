d646a4b2f57295c4b4509545f440d4c7e39ecdf1
Eba-1 <ebenwoldehan@gmail.com>
2023-02-21 10:30:36 -0800

    Task 1 done

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include<main.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+void print_alphabet()
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; c++)
+	{
+		printf("%c", ch);
+	}
+}
