1683bf23a5cac13861bf6003b0ee5c7f5ad76af8
Eba-1 <ebenwoldehan@gmail.com>
2023-02-21 11:16:49 -0800

    Task 3 done

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 /**
  * _islower - check if the character is in lower case or not
  *
- * @ch - the character that will be provided from the main function
+ * @ch: the character that will be provided from the main function
  *
  * Return: 1 if yes, 0 if no
  */
