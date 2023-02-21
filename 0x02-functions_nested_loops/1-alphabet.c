211949a4879122067a6340dcb0e8e09028952020
Eba-1 <ebenwoldehan@gmail.com>
2023-02-21 10:48:02 -0800

    Task 1 done

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
- * main - Entry point
+ * print_alphabet - a function to print lower case alphabets
  *
- * Return: Always 0 (success)
+ * Return: Always 0
  */
-void print_alphabet()
+void print_alphabet(void)
 {
 	char ch;
+
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-		_putchar( ch);
+		_putchar(ch);
 	}
 	_putchar('\n');
 }
