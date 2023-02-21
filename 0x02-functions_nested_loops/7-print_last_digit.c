9047a6dab5fe068a6c67235b42f8fc9a01a92f26
Eba-1 <ebenwoldehan@gmail.com>
2023-02-21 12:05:41 -0800

    Task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,9 +8,18 @@
  */
 int print_last_digit(int n)
 {
+	int last_digit;
+
 	if (n < 0)
 	{
-		return ((0 - n % 10) * 11);
+		last_digit = (-1 * (n % 10));
+		_putchar (last_digit + '0');
+		return (last_digit);
 	}
-	return ((n % 10) * 11);
+	else
+	{
+		last_digit = (n % 10);
+		_putchar (last_digit + '0');
+		return (last_digit);
+	}
 }
