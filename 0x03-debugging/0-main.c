96ca566c389d4bc060d6a5322d9ced7663d42484
Ebenezer Woldehana <95696690+Eba-1@users.noreply.github.com>
2023-02-23 23:01:22 +0530

    Edited the main.c file

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (success)
+  */
+int main(void)
+{
+        int i;
+
+        i = 98;
+        positive_or_negative(i);
+
+        return (0);
+}
