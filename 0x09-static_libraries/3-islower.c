0a4c9d01f4597b6facc8b01f17711d71b05cc3df
Eba-1 <ebenwoldehan@gmail.com>
2023-03-13 20:50:25 -0700

    Required files copied from their respective directory

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _islower - check if the character is in lower case or not
+ *
+ * @ch: the character that will be provided from the main function
+ *
+ * Return: 1 if yes, 0 if no
+ */
+int _islower(int ch)
+{
+	return ('a' <= ch && ch <= 'z');
+}
---
+++ 0x09-static_libraries/_putchar
@@ -0,0 +1,8 @@
+#include<unistd.h>
+/**
+ * a custome function to print characters
+ */
+int _putchar(char c)
+{
+	return(write(1, &c, 1));
+}
