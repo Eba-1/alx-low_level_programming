16fda2e0c8fdb1a7597bb4982a0501f21348a374
Eba-1 <ebenwoldehan@gmail.com>
2023-04-01 00:44:53 -0700

    Update:Betty style and other erros

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
  * add_node - adds a new node at the beginning
  * of a list_t list.
@@ -12,8 +11,8 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *newNode;
 	size_t n;
-	newNode = malloc(sizeof(list_t));
 
+	newNode = malloc(sizeof(list_t));
 	if (newNode == NULL)
 		return (NULL);
 
