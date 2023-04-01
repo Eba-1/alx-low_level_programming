05edbbe121543479ff1032e7cd202c2ee0793cb2
Eba-1 <ebenwoldehan@gmail.com>
2023-03-31 21:54:43 -0700

    Task 1 done and works perfectly

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/*
+ * list_len - A function to find the length of a linked list.
+ *
+ * @h: Parameter (the head of the linked  list)
+ *
+ * Return: Return the number of elements.
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return(count);
+}
