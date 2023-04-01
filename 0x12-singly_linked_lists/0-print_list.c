dc985bd3f75faaacc5349798969e37d30f469348
Eba-1 <ebenwoldehan@gmail.com>
2023-03-31 21:44:17 -0700

    Task 0 complete

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+#include <stdio.h>
+
+/*
+ * print_list - a function that prints all the elements of a list.
+ * 
+ * @h: A parameter (the head of the linked list)
+ * 
+ * Return: The number of elements in the list
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		if (h->str != NULL)
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		else
+		{
+			printf("[0] (nil)\n");
+		}
+		h = h->next;
+	}
+	return(count);
+}
