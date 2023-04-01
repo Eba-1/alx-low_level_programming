d6037c316bcb01f02dc31f9155074c307b3f1f97
Eba-1 <ebenwoldehan@gmail.com>
2023-03-31 23:52:59 -0700

    Update on the style

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,10 +1,7 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
 
 /*
- * add node - Adding a node at the beginning of the list.
+ * add_node - Adding a node at the beginning of the list.
  * @head: Parameter (the head opf the list)
  * @str: The value that the node will have.
  * Return: Address of the new node.
@@ -14,13 +11,13 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *newNode;
 	size_t n;
+
 	newNode = malloc(sizeof(list_t));
-
 	if (newNode == NULL)
+	{
 		return(NULL);
-
-	for (n = 0; str[n]; n++)
-	;
+	}
+	for (n = 0; str[n]; n++);
 
 	newNode->len = n;
 	newNode->str = strdup(str);
