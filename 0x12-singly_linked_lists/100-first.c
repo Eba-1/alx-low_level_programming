b223b315d677942898a5a170930486b0a308ab6c
Eba-1 <ebenwoldehan@gmail.com>
2023-04-02 23:56:22 -0700

    Task 100 done

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * first - Function that will be executed before the main function
+ * Return: Void function with no returns
+ */
+void __attribute__ ((constructor)) first()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
