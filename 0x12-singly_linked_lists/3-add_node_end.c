6539719ad543779e3b6953dea6bcc31989797b9d
Eba-1 <ebenwoldehan@gmail.com>
2023-04-02 23:49:33 -0700

    Update on Betty style

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -8,7 +8,7 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	size_t size;
-	list_t current, *newNode;
+	list_t *current, *newNode;
 
 	newNode = malloc(sizeof(list_t));
 	if (newNode == NULL)
