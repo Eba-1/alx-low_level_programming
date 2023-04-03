3e572694066fd497780219b14d078e21a232779d
Eba-1 <ebenwoldehan@gmail.com>
2023-04-03 00:03:56 -0700

    Task 3 done

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,10 +1,12 @@
 #include "lists.h"
+
 /**
  * add_node_end - Adding a node at the beginning of the list.
  * @head: Parameter (the head opf the list)
  * @str: The value that the node will have.
  * Return: Address of the new node.
  */
+
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *current, *newNode;
