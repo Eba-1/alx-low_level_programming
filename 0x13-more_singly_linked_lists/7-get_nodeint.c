1e46a6d2d8aeb45abd30e8544f75d63da1528a25
Ebenezer Woldehana <95696690+Eba-1@users.noreply.github.com>
2023-04-05 03:01:37 +0530

    Update 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -16,7 +16,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	while (current != NULL && index > 0)
 	{
 		current = current->next;
-		index;
+		index--;
 	}
 	return (current);
 }
