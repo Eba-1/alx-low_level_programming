7cf8a968271125c36d89a1a70643a32b061fff28
Ebenezer Woldehana <95696690+Eba-1@users.noreply.github.com>
2023-04-04 23:33:19 +0530

    File 2 created and compilation was successful

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * listint_len - Function that returns the number of
+ * elements of a listint_t list.
+ *
+ * @h: Parameter (the head of the list)
+ *
+ * Return: Returns the number of nodes.
+ */
+
+size_t listint_len(const listint_t *h)
+{
+        size_t size = 0;
+        const listint_t *current = h;
+
+        while (current != NULL)
+        {
+                current = current->next
+                size++;
+        }
+        return (size);
+}
