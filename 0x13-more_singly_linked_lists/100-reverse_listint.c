2c38bf921e3448d66277ec03ac19be0bfba7da8e
Eba-1 <ebenwoldehan@gmail.com>
2023-04-04 16:04:48 -0700

    optional tasks

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - Function that deletes nde at index
+ * @head: Parameter (the head of the list)
+ * @index: Parameter (the index of the node to delete
+ * Return: 1 for success, -1 for failure
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i;
+	listint_t *current = *head;
+	
+	if (*head == NULL)
+	{
+		return (-1);
+	}
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		return (1);
+	}
+	for (i = 1; i < index; i++)
+	{
+		current = current->next;
+	}
+	current->next = current->next->next;
+	return (1);
+}
