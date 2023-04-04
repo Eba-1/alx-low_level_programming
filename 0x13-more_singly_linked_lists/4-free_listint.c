57fe6137aaa550952f61d9cdd9fe5b7e848b7519
Ebenezer Woldehana <95696690+Eba-1@users.noreply.github.com>
2023-04-05 00:56:07 +0530

    Update 4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -8,12 +8,12 @@
 
 void free_listint(listint_t *head)
 {
-	listint_t *current;
+	listint_t *current = *head;
 
-	while ((current = head) != NULL)
+	while (current != NULL)
 	{
-		head = head->next;
 		free(current->n);
 		free(current);
+		current = current->next;
 	}
 }
