b0ea5adf67a68fe03d5f2c8147e6f8def31a1e63
Ebenezer Woldehana <95696690+Eba-1@users.noreply.github.com>
2023-04-05 01:00:49 +0530

    Create 5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * freeint_list2 - A function that frees a list
+ * @head: Parameter (head of the linked list).
+ * Return: Void functions don't return anything.
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+
+	while (*head != NULL)
+	{
+		current = *head;		
+		*head = (*head)->next;
+		free(current);
+	}
+  *head = NULL;
+}
