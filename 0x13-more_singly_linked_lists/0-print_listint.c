8e585caf70fbfdff13cdfbc2a7721c276db1e0e9
Ebenezer Woldehana <95696690+Eba-1@users.noreply.github.com>
2023-04-05 02:39:02 +0530

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,12 +9,11 @@ size_t print_listint(const listint_t *h)
 {
 	size_t size = 0;
 	const listint_t *current = h;
-	
 	while (current != NULL)
 	{
 		printf("%d\n", current->n);
-		size++;
 		current = current->next;
-	}	
+		size++;
+	}
 	return (size);
 }
