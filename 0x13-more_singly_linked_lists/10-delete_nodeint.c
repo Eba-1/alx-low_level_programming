7869faa3ca17f25e56af13ed9fbe1c06909ac727
Ebenezer Woldehana <95696690+Eba-1@users.noreply.github.com>
2023-04-05 04:21:47 +0530

    Update 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -21,7 +21,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		*head = (*head)->next;
 		return (1);
 	}
-	for (i = 1; i < index - 1; i++)
+	for (i = 1; i < index; i++)
 	{
 		if (current->next == NULL)
 		{
