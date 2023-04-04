7b193f88e7fac1827d2b8de938e76a9d35556f04
Eba-1 <ebenwoldehan@gmail.com>
2023-04-04 12:09:22 -0700

    File 4 created

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,1 +1,43 @@
 #include "lists.h"
+
+/**
+ * add_node_end - Adding a node at the beginning of the list.
+ * @head: Parameter (the head opf the list)
+ * @str: The value that the node will have.
+ * Return: Address of the new node.
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *current, *newNode;
+	size_t n;
+
+	newNode = malloc(sizeof(list_t));
+	if (newNode == NULL)
+	{
+		return (NULL);
+	}
+
+	newNode->str = strdup(str);
+
+	for (n = 0; str[n]; n++)
+		;
+
+	newNode->len = n;
+	newNode->next = NULL;
+	current = *head;
+
+	if (current == NULL)
+	{
+		*head = newNode;
+		return (*head);
+	}	
+	while (current->next != NULL)
+	{
+		current = current->next;
+	}
+
+	current->next = newNode;
+
+	return (*head);
+}
