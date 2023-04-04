6d6dde1427c08bab1565f6fd6251974490232a12
Ebenezer Woldehana <95696690+Eba-1@users.noreply.github.com>
2023-04-05 04:39:59 +0530

    Update 100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,30 +1,25 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - Function that deletes nde at index
+ * reverse_listint - Function that reverses a list
  * @head: Parameter (the head of the list)
- * @index: Parameter (the index of the node to delete
- * Return: 1 for success, -1 for failure
  */
 
-int delete_nodeint_at_index(listint_t **head, unsigned int index)
+listint_t *reverse_listint(listint_t **head)
 {
-	unsigned int i;
-	listint_t *current = *head;
+	listint_t *current = *head, *previous = NULL, next_node = head;
 	
-	if (*head == NULL)
+	while (current != NULL)
 	{
-		return (-1);
+		next_node = current->next;
+		current->next = previous;
+		previous = current;
+		current = next;
 	}
 	if (index == 0)
 	{
 		*head = (*head)->next;
 		return (1);
 	}
-	for (i = 1; i < index; i++)
-	{
-		current = current->next;
-	}
-	current->next = current->next->next;
-	return (1);
+	return (previous);
 }
