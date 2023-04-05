5104589fdf9e94ea9bd7826e3087124b9d3ecbf6
Ebenezer Woldehana <95696690+Eba-1@users.noreply.github.com>
2023-04-05 05:42:45 +0530

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,10 +1,7 @@
 #include "lists.h"
 /**
- * listint_len - Function that returns the number of
- * elements of a listint_t list.
- *
+ * listint_len - Function that returns the number of elements.
  * @h: Parameter (the head of the list)
- *
  * Return: Returns the number of nodes.
  */
 size_t listint_len(const listint_t *h)
