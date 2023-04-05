90ad8c831d3835765610203c83203b2516893152
Ebenezer Woldehana <95696690+Eba-1@users.noreply.github.com>
2023-04-05 05:38:50 +0530

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,11 +1,9 @@
 #include "lists.h"
-
 /**
- * print_listint - Function that prints all elements
- * @h: Parameter (the head of the list)
- * Return: Returns the number of nodes
+ * print_listint - Function that prints all elements.
+ * @h: Parameter which is the head of the list.
+ * Return: Returns the number of nodes.
  */
-
 size_t print_listint(const listint_t *h)
 {
 	size_t size = 0;
