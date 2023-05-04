c6155de35032d49428e105f93ed92e2b728c332c
Eba-1 <ebenwoldehan@gmail.com>
2023-05-04 10:33:30 -0700

    Task 5 done

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * flip_bits - Function that returns the number of bits to be flipped.
+ * @n: Parameter (First number).
+ * @m: Parameter (Second number).
+ * Return: Number of bits needed to flip number.
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int mask = n ^ m, counter = 0;
+
+	while (mask > 0)
+	{
+		counter++;
+		mask &= (mask - 1);
+	}
+	return (counter);
+}
