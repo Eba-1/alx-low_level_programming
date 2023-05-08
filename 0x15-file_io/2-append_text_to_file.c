dffa566187aeb7bae8773bb3d190f6a2a120f378
Ebenezer Woldehana <95696690+Eba-1@users.noreply.github.com>
2023-05-08 23:40:45 +0530

    Task 2 done

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,7 +14,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
         if (!filename)
                 return (-1);
-        check = open(filename, O_WRONLY | O_APPEND);
+        check = open(filename, O_RDWR | O_APPEND);
 	if (check == -1)
                 return (-1);
         if (text_content)
