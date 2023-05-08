7baff95ba5a08c814773dbaee8e52c26746890c5
Ebenezer Woldehana <95696690+Eba-1@users.noreply.github.com>
2023-05-09 00:13:05 +0530

    Task 3 done

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,7 +17,7 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	src = open(argv[1], O_RD0NLY);
+	src = open(argv[1], O_RDONLY);
 	if (src == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
