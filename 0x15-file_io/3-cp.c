02ae78e183afec4a84a745b25cc5a846c411b963
Eba-1 <ebenwoldehan@gmail.com>
2023-05-08 11:35:26 -0700

    Task 3 done

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,59 @@
+#include "main.h"
+
+/**
+ * main - Entry point.
+ * @argc: Parameter (the number of arguments).
+ * @argv: Parameter (argument vector).
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	int src, dest;
+	char str[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	src = open(argv[1], 0_RD0NLY);
+	if (src == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+			argv[1]);
+		exit(98);
+	}
+	dest = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (dest == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	while (letter == 1024)
+	{
+		letter = read(src, str, 1024);
+		if (letter == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+		letter_w = write(dest, str, letter);
+		if (letter_w == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+	if (close(src) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd  %d\n", file_from);
+		exit(100);
+	}
+	if (close(dest) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd  %d\n", file_to);
+		exit(100);
+	}
+	return (0);
+}
